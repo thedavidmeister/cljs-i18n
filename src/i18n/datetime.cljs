e5dfc1ec6862a8e93b32bcd80ad04b5fbdee8fe7
David Meister <thedavidmeister@gmail.com>
2018-03-24 18:20:45 +1100

    tests and docs for datetime parsing and formatting

--- src/i18n/datetime.cljs
+++ src/i18n/datetime.cljs
@@ -52,9 +52,7 @@
 
 (defn -format
  [d & {:keys [locale pattern tz]}]
- {:pre [
-        ; (time.data/date-time? d)
-        (string? locale)]
+ {:pre [(string? locale)]
   :post [(string? locale)]}
  (let [locale (or locale i18n.data/default-locale)
        pattern (or pattern default-pattern)]
@@ -71,8 +69,7 @@
  {:pre [(string? s) (string? locale)]
   :post []}
          ; (time.data/date-time? %)]}
- (let [locale (or locale i18n.data/default-locale)
-       pattern (or pattern default-pattern)]
+ (let [locale (or locale i18n.data/default-locale)]
   (i18n.goog/set-locale! locale)
   (let [d (js/Date.)]
    (.parse
@@ -89,23 +86,66 @@
  (is (identical? goog.i18n.DateTimeSymbols_en_AU (locale->symbols "en-AU")))
  (is (identical? goog.i18n.DateTimeSymbols_en (locale->symbols "asdf"))))
 
+(deftest ??timezone
+ (=
+  {:id "Etc/GMT-10"
+   :std_offset 600
+   :names ["UTC+10" "UTC+10"]
+   :names_ext {:STD_LONG_NAME_GMT "GMT+10:00"
+               :STD_GENERIC_LOCATION "GMT+10:00"
+               :transitions []}}
+  (js->clj
+   (.getTimeZoneData (timezone -600))
+   :keywordize-keys true)))
+
+(deftest ??format
+ (is (= "May 12, 1973" (format (js/Date. 106000000000) :locale "en-US")))
+ (is (= "12 May 1973" (format (js/Date. 106000000000) :locale "en-AU")))
+
+ (is (= "6:26 AM" (format (js/Date. 106000000000) :locale "en-US" :pattern :short-time)))
+ (is (= "6:26 am" (format (js/Date. 106000000000) :locale "en-AU" :pattern :short-time)))
+
+ (is
+  (=
+   "Saturday, May 12, 1973 at 6:26:40 AM UTC+10"
+   (format (js/Date. 106000000000) :locale "en-US" :pattern :full-datetime)))
+ (is
+  (=
+   "Saturday, 12 May 1973 at 6:26:40 am UTC+10"
+   (format (js/Date. 106000000000) :locale "en-AU" :pattern :full-datetime)))
+
+ (is
+  (=
+   "Saturday, 12 May 1973 at 6:26:40 am UTC+10"
+   (format (js/Date. 106000000000) :locale "en-AU" :pattern :full-datetime :tz -600))))
+
+(deftest ??parse
+ (=
+  #inst "1973-05-12T08:09:46.875-00:00"
+  (parse "May 12, 1973" :locale "en-US" :pattern :long-date))
+ (=
+  #inst "2018-03-24T07:09:46.878-00:00"
+  (parse "5/11/73" :locale "en-US" :pattern :long-date)))
+
 (deftest ??format-parse
  ; May 12 1973.
- (let [d (js/Date. 106000000000)
+ (let [stamp 106000000000
+       ds [(js/Date. stamp) (doto (goog.date.DateTime.) (.setTime stamp))]
        tests [["en-US" "5/11/73"]
               ["en-AU" "11/5/73"]]
        tests' (shuffle tests)]
   ; Do everything twice to flush out cache issues.
-  (dotimes [n 2]
-   (taoensso.timbre/debug "Locale date format test run: " (inc n))
+  (doseq [d ds]
+   (dotimes [n 2]
+    (taoensso.timbre/debug "Locale date format test run: " (inc n))
 
-   (doseq [[l f] tests']
-    (taoensso.timbre/debug "Checking locale: " l)
-    (let [s (format d :locale l :pattern :short-date :tz 0)]
-     (is (= f s)
-         (str "Failed to format " l " to " f " correctly." s)))
-    (let [p (parse f :locale l :pattern :short-date)]
-     (is (== 1973 (.getFullYear p)))
-     ; JS months are 0 indexed.
-     (is (== 4 (.getMonth p)))
-     (is (== 11 (.getDate p))))))))
+    (doseq [[l f] tests']
+     (taoensso.timbre/debug "Checking locale: " l)
+     (let [s (format d :locale l :pattern :short-date :tz 0)]
+      (is (= f s)
+          (str "Failed to format " l " to " f " correctly." s)))
+     (let [p (parse f :locale l :pattern :short-date)]
+      (is (== 1973 (.getFullYear p)))
+      ; JS months are 0 indexed.
+      (is (== 4 (.getMonth p)))
+      (is (== 11 (.getDate p)))))))))
