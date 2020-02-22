5f8696a5460aacee67f1e20c8712c5a8ed5ac600
Toropenko Sergey <i.m.just.a.humble.tester@gmail.com>
2020-02-22 20:53:40 +0200

    Fixes #57

--- src/i18n/spec.cljs
+++ src/i18n/spec.cljs
@@ -1,26 +1,33 @@
 (ns i18n.spec
- (:require
-  [clojure.spec.alpha :as spec]
-  i18n.data
-  i18n.locale
-  [cljs.test :refer-macros [deftest is]]))
+	(:require
+		[clojure.spec.alpha :as spec]
+		i18n.data
+		i18n.locale
+		[cljs.test :refer-macros [deftest is testing]]))
 
 (spec/def :i18n/code i18n.locale/valid-locale?)
+(spec/def :i18n/description (spec/and string? seq))
 
 (spec/def :i18n/locale
- (spec/keys
-  :req [:i18n/number-format-symbols
-        :i18n/number-format-symbols-latin
-        :i18n/number-format-symbols-compact
-        :i18n/date-time-symbols
-        :i18n/date-time-patterns
-        :i18n/code]))
+	(spec/keys
+		:req [:i18n/description
+			  :i18n/number-format-symbols
+			  :i18n/number-format-symbols-latin
+			  :i18n/number-format-symbols-compact
+			  :i18n/date-time-symbols
+			  :i18n/date-time-patterns
+			  :i18n/code]))
 
 (spec/def :i18n/locales
- (spec/map-of :i18n/code :i18n/locale))
+	(spec/map-of :i18n/code :i18n/locale))
 
 (deftest ??locales
- (let [locales (dissoc i18n.data/locales :default)]
-  (is
-   (spec/valid? :i18n/locales locales)
-   (spec/explain-str :i18n/locales locales))))
+	(let [locales (dissoc i18n.data/locales :default)]
+		(is
+			(spec/valid? :i18n/locales locales)
+			(spec/explain-str :i18n/locales locales))
+
+		(testing "all language descriptions are distinct"
+			(let [descriptions (->> locales vals (map :i18n/description))]
+				(is (= (sort (distinct descriptions))
+					   (sort descriptions)))))))
